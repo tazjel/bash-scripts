37393805381905505b7164264711c91f74160d4a
Luca B <blmlcu@gmail.com>
2013-09-01 09:55:52 +0100

    Add initial version of xfce setup

---
+++ xfce.sh
@@ -0,0 +1,38 @@
+#/usr/bin/env bash
+
+sudo apt-get remove --purge \
+  gnome-system-tools \
+  gnome-time-admin \
+  indicator-application-gtk2 \
+  indicator-messages-gtk2 \
+  indicator-sound-gtk2 \
+  lightdm-gtk-greeter \
+  link-grammar-dictionaries-en \
+  pavucontrol \
+  plymouth-theme-xubuntu-logo \
+  plymouth-theme-xubuntu-text \
+  screensaver-default-images \
+  shimmer-themes \
+  system-tools-backends \
+  ttf-droid \
+  ttf-lyx \
+  xfburn \
+  xfce4-dict \
+  xfce4-indicator-plugin \
+  xfce4-power-manager \
+  xfce4-power-manager-data \
+  xfce4-screenshooter \
+  xfce4-systemload-plugin \
+  xfce4-taskmanager \
+  xfce4-xkb-plugin \
+  xubuntu-artwork \
+  xubuntu-default-settings \
+  xubuntu-icon-theme \
+  xubuntu-wallpapers \
+  libgranite0 \
+  indicator-applet-appmenu \
+  firefox-globalmenu \
+  # appmenu-* \
+  # indicator-appmenu* \
+
+
